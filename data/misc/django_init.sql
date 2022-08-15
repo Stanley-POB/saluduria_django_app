1541a89adbcb9086aa2986fbd75097e497eb24eb
pierreobeau <beauliver@gmail.com>
2022-08-15 16:55:40 +0200

    second commit

---
+++ data/misc/django_init.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS flyway_db;
+
+CREATE DATABASE flyway_db;
+
+\ c flyway_db
+SET
+    statement_timeout = 0;
+
+SET
+    lock_timeout = 0;
+
+SET
+    client_encoding = 'UTF8';
+
+SET
+    standard_conforming_strings = on;
+
+SET
+    check_function_bodies = false;
+
+SET
+    client_min_messages = warning;
+
+SET
+    default_tablespace = '';
+
+SET
+    default_with_oids = false;
